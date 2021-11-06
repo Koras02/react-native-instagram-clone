b065cdb60dbaa9c25456813e085366ee5001c9e0
Koras02 <wjdgh0727@gmail.com>
2021-11-06 23:52:35 +0900

    Story 와 Header 생성 완료 54:45

---
+++ data/posts.js
@@ -0,0 +1,66 @@
+import { USERS } from './users';
+
+export const POSTS = [
+    {
+        imageurl: 'https://i.ibb.co/182bP1y/4k.png',
+        user: USERS[0].user,
+        likes: 7870,
+        caption: 'Train Ride to Hogwarts.',
+        photite_picture: USERS[0].image,
+        comments: [
+            {
+                user: 'theqazman',
+                comment: 'Wow! This build looks fire, Super excited about',
+            },
+            {
+                user: 'amaanth.dev',
+                comment: "Once I wake up, I'll finally be ready to code this up!",
+            },
+        ],
+        imageurl: 'https://i.ibb.co/182bP1y/4k.png',
+        user: USERS[0].user,
+        likes: 7870,
+        caption: 'Train Ride to Hogwarts.',
+        photite_picture: USERS[0].image,
+        comments: [
+            {
+                user: 'theqazman',
+                comment: 'Wow! This build looks fire, Super excited about',
+            },
+            {
+                user: 'amaanth.dev',
+                comment: "Once I wake up, I'll finally be ready to code this up!",
+            },
+        ],
+        imageurl: 'https://i.ibb.co/182bP1y/4k.png',
+        user: USERS[0].user,
+        likes: 7870,
+        caption: 'Train Ride to Hogwarts.',
+        photite_picture: USERS[0].image,
+        comments: [
+            {
+                user: 'theqazman',
+                comment: 'Wow! This build looks fire, Super excited about',
+            },
+            {
+                user: 'amaanth.dev',
+                comment: "Once I wake up, I'll finally be ready to code this up!",
+            },
+        ],
+        imageurl: 'https://i.ibb.co/182bP1y/4k.png',
+        user: USERS[0].user,
+        likes: 7870,
+        caption: 'Train Ride to Hogwarts.',
+        photite_picture: USERS[0].image,
+        comments: [
+            {
+                user: 'theqazman',
+                comment: 'Wow! This build looks fire, Super excited about',
+            },
+            {
+                user: 'amaanth.dev',
+                comment: "Once I wake up, I'll finally be ready to code this up!",
+            },
+        ],
+    },
+];
